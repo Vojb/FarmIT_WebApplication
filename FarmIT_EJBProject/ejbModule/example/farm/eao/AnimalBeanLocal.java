5dd1956c411cd0a64dfdebda027cd0fba0930ab1
bratendal <erik.bratendal@gmail.com>
2016-02-03 15:28:43 +0100

    added methods to animalbeanLocal

--- FarmIT_EJBProject/ejbModule/example/farm/eao/AnimalBeanLocal.java
+++ FarmIT_EJBProject/ejbModule/example/farm/eao/AnimalBeanLocal.java
@@ -2,7 +2,17 @@ package example.farm.eao;
 
 import javax.ejb.Local;
 
+import example.farm.model.Animal;
+
 @Local
 public interface AnimalBeanLocal {
 
+	public Animal findByIdAnimal();
+	
+
+	public Animal createEmployee();
+
+	public Animal updateEmployee();
+
+	public void deleteEmployee();
 }
