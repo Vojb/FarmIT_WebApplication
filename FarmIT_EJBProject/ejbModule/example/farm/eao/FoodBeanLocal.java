34d830d875cfc7b0735016bfc5c1b52883dfbf94
Vojb <Bruuhn@hotmail.com>
2016-02-04 13:08:36 +0100

    HAR JÄVLA NUPPOINTET

--- FarmIT_EJBProject/ejbModule/example/farm/eao/FoodBeanLocal.java
+++ FarmIT_EJBProject/ejbModule/example/farm/eao/FoodBeanLocal.java
@@ -2,7 +2,12 @@ package example.farm.eao;
 
 import javax.ejb.Local;
 
+import example.farm.model.Food;
+
 @Local
 public interface FoodBeanLocal {
-
+	public Food findByIdFood(long id);
+	public Food createFood(Food f);
+	public Food updateFood(Food f);	
+	public void deleteFood(long id);
 }
--- FarmIT_EJBProject/ejbModule/example/farm/facade/FarmFacade.java
+++ FarmIT_EJBProject/ejbModule/example/farm/facade/FarmFacade.java
@@ -5,7 +5,9 @@ import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 
 import example.farm.eao.AnimalBean;
+import example.farm.eao.FoodBean;
 import example.farm.model.Animal;
+import example.farm.model.Food;
 
 /**
  * Session Bean implementation class FarmFacade
@@ -16,24 +18,44 @@ public class FarmFacade implements FarmFacadeLocal {
 
 	@EJB
 	AnimalBean ab;
-	
-    /**
-     * Default constructor. 
-     */
-    public FarmFacade() {
-    	// TODO Auto-generated constructor stub
-    }
-    public Animal createAnimal(Animal a){
-    	return ab.createAnimal(a);
-    }
-    public Animal updateAnimal(Animal a){
-    	return ab.updateAnimal(a);
-    }
-    public void deleteAnimal(long a){
-    	 ab.deleteAnimal(a);
-    }
-    public Animal findByIdAnimal(long a){
-    	return ab.findByIdAnimal(a);
-    }
+	@EJB
+	FoodBean fb;
+
+	/**
+	 * Default constructor.
+	 */
+	public FarmFacade() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Animal createAnimal(Animal a) {
+		return ab.createAnimal(a);
+	}
+
+	public Animal updateAnimal(Animal a) {
+		return ab.updateAnimal(a);
+	}
+
+	public void deleteAnimal(long a) {
+		ab.deleteAnimal(a);
+	}
+
+	public Animal findByIdAnimal(long a) {
+		return ab.findByIdAnimal(a);
+	}
+
+	public Food findByIdFood(long id){
+		return fb.findByIdFood(id);
+	}
+	public Food createFood(Food f){
+		return fb.createFood(f);
+	}
+
+	public Food updateFood(Food f){
+		return fb.updateFood(f);
+	}
+	public void deleteFood(long id){
+		fb.deleteFood(id);
+	}
 
 }
