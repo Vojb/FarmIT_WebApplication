a97c191c9dbceb3cfa7b8babc9ac0fe3b1cf4da8
Vojb <Bruuhn@hotmail.com>
2016-02-04 13:36:57 +0100

    emedebebefded

--- FarmIT_EJBProject/ejbModule/example/farm/model/AnimalFoodId.java
+++ FarmIT_EJBProject/ejbModule/example/farm/model/AnimalFoodId.java
@@ -1,21 +1,56 @@
 package example.farm.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
 @Embeddable
 public class AnimalFoodId {
-	
+
 	private long idFood;
 	private long idAnimal;
-	public long getIdFood() {
+
+	@Column(name = "idFood", nullable = false)
+	public Long getIdFood() {
 		return idFood;
 	}
+
 	public void setIdFood(long idFood) {
 		this.idFood = idFood;
 	}
-	public long getIdAnimal() {
+
+	@Column(name = "idAnimal", nullable = false)
+	public Long getIdAnimal() {
 		return idAnimal;
 	}
-	public void setIdAnimal(long idAnimal) {
+
+	public void setIdAnimal(Long idAnimal) {
 		this.idAnimal = idAnimal;
+	}
+
+	public AnimalFoodId(long idAnimal, long idFood) {
+		this.idAnimal = idAnimal;
+		this.idFood = idFood;
+	}
+
+	public boolean equals(Object other) {
+		if ((this == other)) {
+			return true;
+		}
+		if ((other == null)) {
+			return false;
+		}
+		if (!(other instanceof AnimalFoodId)) {
+			return false;
+		}
+		AnimalFoodId castOther = (AnimalFoodId) other;
+		return ((this.getIdFood() == castOther.getIdFood()) || (this.getIdFood() != null
+				&& castOther.getIdFood() != null && this.getIdFood() == (castOther.getIdFood())))
+				&& ((this.getIdAnimal() == castOther.getIdAnimal()) || (this.getIdAnimal() != null
+						&& castOther.getIdAnimal() != null && this.getIdAnimal() == (castOther.getIdAnimal())));
+	}
+
+	public int hashCode() {
+		return super.hashCode();
 	}
 
 }
