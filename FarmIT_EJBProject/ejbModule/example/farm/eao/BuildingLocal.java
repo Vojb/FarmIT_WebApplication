3b51f6e116a61f90d90d1fcc051331fe0663ffa0
Vojb <Bruuhn@hotmail.com>
2016-02-05 10:24:22 +0100

    losat

--- FarmIT_EJBProject/ejbModule/example/farm/eao/BuildingLocal.java
+++ FarmIT_EJBProject/ejbModule/example/farm/eao/BuildingLocal.java
@@ -4,5 +4,10 @@ import javax.ejb.Local;
 
 @Local
 public interface BuildingLocal {
+	
+	public Building findByIdBuilding(long id);
 
+	public Building createBuilding(Building a);
+
+	public Building updateBuilding(Building a);
 }
