f57c5b27d852e0c27fcb78b5ce00011946dc4cb6
bratendal <erik.bratendal@gmail.com>
2016-02-09 14:43:00 +0100

    Farmletfunkar :3

--- FarmClient/src/example/farm/Servlets/Farmlet.java
+++ FarmClient/src/example/farm/Servlets/Farmlet.java
@@ -1,4 +1,4 @@
-	package example.farm.Servlets;
+package example.farm.Servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -48,61 +48,57 @@ public class Farmlet extends HttpServlet {
 		out.println("</head><body>");
 		out.println("<h3>Farmlet</h3>");
 
-//		 Cow a = new Cow();
-//		 a.setAge("12");
-//		 a.setIdAnimal(2);
-//		 a.setName("Sosse");
-//		 a.setStatusAnimal("Alive");
-		// facade.createAnimal(a);	
-		
-//		Building bs=new Building ();
-//		bs.setIdBuilding("Alfa");
-//		facade.createBuilding(bs);
-//		
-//		
-//		Animal a = facade.findByIdAnimal(2);
-//	
-		Building e1 = facade.findByIdBuilding("Alfa");
-		 if (e1 != null) {
-		out.println("<h4>Hittade: "+ e1.getIdBuilding());
-		out.println(" </h4>");
-		 }
-		 Box e3 = facade.findByIdBox("1");
-		 if (e3 != null) {
-		out.println("<h4>Hittade: "+ e3.getIdBox());
-		out.println(" "+e3.getBuilding()+ "</h4>");
-		 }
-		 Animal e2 = facade.findByIdAnimal(10001);
-		 if (e2 != null) {
-		out.println("<h4>Hittade: "+ e2.getName());
-		out.println(" "+e2.getStatusAnimal()+ "</h4>");
-		 }
-		 Food e4 = facade.findByIdFood(1);
-		 if (e4 != null) {
-		out.println("<h4>Hittade: "+ e4.getType());
-		out.println(" </h4>");
-		 }
-		
-		 
-	
-		 List<Animal> animals = facade.findAll();
-		 for (Animal emp2 : animals) {
-		 out.println("<h4>Hittade: "+ emp2.getName());
-		 out.println(" Id: "+ emp2.getIdAnimal());
-		 out.println("</h4>");
-		 }
+		Cow a = new Cow();
+		a.setAge("12");
+		a.setIdAnimal(2);
+		a.setName("Sosse");
+		a.setStatusAnimal("Alive");
+		facade.createAnimal(a);
 
-		 
-////		
-//		Animal a = facade.findByIdAnimal(10001);
-//		Building bs =facade.findByIdBuilding("FuckOF");
+		Building bs=new Building ();
+		bs.setIdBuilding("Alfa");
+		facade.createBuilding(bs);
+		
+		
+		// Animal a = facade.findByIdAnimal(2);
+		//
+//		Building e1 = facade.findByIdBuilding("Alfa");
+//		if (e1 != null) {
+//			out.println("<h4>Hittade: " + e1.getIdBuilding());
+//			out.println(" </h4>");
+//		}
+//		Box e3 = facade.findByIdBox("1");
+//		if (e3 != null) {
+//			out.println("<h4>Hittade: " + e3.getIdBox());
+//			out.println(" " + e3.getBuilding() + "</h4>");
+//		}
+//		Animal e2 = facade.findByIdAnimal(10001);
+//		if (e2 != null) {
+//			out.println("<h4>Hittade: " + e2.getName());
+//			out.println(" " + e2.getStatusAnimal() + "</h4>");
+//		}
+//		Food e4 = facade.findByIdFood(1);
+//		if (e4 != null) {
+//			out.println("<h4>Hittade: " + e4.getType());
+//			out.println(" </h4>");
+//		}
+
+		List<Animal> animals = facade.findAll();
+		for (Animal emp2 : animals) {
+			out.println("<h4>Hittade: " + emp2.getName());
+			out.println(" Id: " + emp2.getIdAnimal());
+			out.println("</h4>");
+		}
+
+		////
+		// Animal a = facade.findByIdAnimal(10001);
+		// Building bs =facade.findByIdBuilding("FuckOF");
 
 //		Box b = new Box();
 //		b.setIdBox("1");
 //		b.setBuilding(e1);
 //		facade.createBox(b);
-////		
-//		
+//
 //		Cow c = new Cow();
 //		c.setBox(e3);
 //		c.setAge("12");
@@ -111,24 +107,23 @@ public class Farmlet extends HttpServlet {
 //		c.setIdAnimal(10001);
 //		c.setStatusAnimal("Frisk");
 //		facade.createAnimal(c);
-		
-//		b.addAnimaltoBox(c);
-		
-//		facade.createBox(b);
-//		
-//		Food f= new Food();
-//		f.setAmount(1000000);
-//		f.setIdFood(1);
-//		f.setType("PowerFeed");
-////	
 
-//		
-		
-			facade.addAnimal(e4, e2);
-//			e4.addAnimal(e2);
-//			e4.getAnimals();
-			facade.updateFood(e4);
+		// b.addAnimaltoBox(c);
 
+		// facade.createBox(b);
+		//
+		// Food f= new Food();
+		// f.setAmount(1000000);
+		// f.setIdFood(1);
+		// f.setType("PowerFeed");
+		////
+
+		//
+//
+//		facade.addAnimal(e4, e2);
+//		// e4.addAnimal(e2);
+//		// e4.getAnimals();
+//		facade.updateFood(e4);
 
 		out.println("</body></html>");
 	}
