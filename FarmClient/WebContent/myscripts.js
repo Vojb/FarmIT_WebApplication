de9e67da8999055d20ce0baf67f8177ed1f2dafb
bratendal <erik.bratendal@gmail.com>
2016-03-02 11:39:31 +0100

    jhjh

--- FarmClient/WebContent/myscripts.js
+++ FarmClient/WebContent/myscripts.js
@@ -4,45 +4,14 @@ $("#changeFood").change(function() {
 		
 	}).change();
 
-//$(document).ready(function(){	
-//	console.log("alsjd")
-//$("#find-animal").click(function(){
-//	alert($('#animalstatus').val());
-//	  $('#foundstatusanimal').val($('#animalstatus').val());
-//		alert($('#animalstatus').val());
-//		alert("fuck")
-//	});	
-//});
-
-/* function x(){
+function x(){
 	var fnElement = document.getElementById("findIdAnimal");
 	  var findIdAnimal = fnElement.value;
+	  alert(findIdAnimal);
 	  if (isNaN(findIdAnimal)) {
-		  
-		  //document.getElementById("findIdAnimal").value = '';		 
-		  //fnElement.setAttribute("placeholder","Id only consists of numbers");
+		  fnElement.setAttribute("placeholder","Id only consists of numbers");
 		  return false;
 	  }
 		document.getElementById("y").submit();
-} */
+}
 
-
-	
-
-//	
-//	function validateForm() {
-//		var fnElement = document.getElementById("findIdAnimal");
-//		var findIdAnimal = fnElement.value;
-//		if (findIdAnimal == null || findIdAnimal == "" || findIdAnimal === "undefined"){
-//		fnElement.setAttribute("placeholder","Animal Id please" );
-//	}
-//		function checkIfIdNbr(){
-//		  var fnElement = document.getElementById("findIdAnimal");
-//		  console.log(fnElement);
-//		  var findIdAnimal = fnElement.value;
-//		  if (isNaN(findIdAnimal)) {
-//			fnElement.setAttribute("placeholder","Id only consists of numbers");
-//		    return false;
-//		  }
-//	}
-//	
